9e74a8a21aa12dd86d12db3fdada9016a9f9a246
vjnambi <vjnambi@gmail.com>
2023-03-13 21:28:15 -0700

    removed unused imports

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,6 @@
 import Banner from '../components/Banner'
 import { useLoaderData } from 'react-router-dom'
-import axios from 'axios';
+//import axios from 'axios';
 
 export async function loader({params}){
     const rawData = [{"id":1,"title":"Test Title","text":"Test Text","createdAt":"2023-03-05T03:06:57.000Z","updatedAt":"2023-03-05T03:06:57.000Z"},{"id":2,"title":"Test Title 2","text":"Test Text 2","createdAt":"2023-03-05T03:08:24.000Z","updatedAt":"2023-03-05T03:08:24.000Z"}]//await axios.get(`http://localhost:3001/article/${params.articleId}`)
